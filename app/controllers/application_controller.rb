ea8fc6e99cacd32010b026452482307706360153
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2020-01-04 03:03:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,49 +9,70 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get "/" do
+	 # renders an index.erb file with links to signup or login
 		erb :index
 	end
 
 	get "/signup" do
+	  # renders a form to create a new user
 		erb :signup
 	end
 
 	post "/signup" do
 		#your code here!
+    user = User.new(:username => params[:username], :password => params[:password])
+   
+   # won't be saved to the db unless our user filled out the password field
+    if user.save
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
 	end
 
 	get "/login" do
+	 # renders a form for logging in
 		erb :login
 	end
 
-	post "/login" do
-		#your code here!
-	end
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+   
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/success"
+    else
+      redirect "/failure"
+    end
+  end
 
 	get "/success" do
+	  # renders a success.erb page
+	  # displayed once user successfully logs in
 		if logged_in?
-			erb :success
-		else
-			redirect "/login"
+		  # @username = current_user.username
+		  erb :success
 		end
 	end
 
 	get "/failure" do
+	  # accessed if there is an error logging in or signing up
 		erb :failure
 	end
 
 	get "/logout" do
+	 # clears the session data and redirects to the homepage
 		session.clear
 		redirect "/"
 	end
 
 	helpers do
 		def logged_in?
-			!!session[:id]
+			!!session[:user_id]
 		end
 
 		def current_user
-			User.find(session[:id])
+			@user = User.find(session[:user_id])
 		end
 	end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-	
+  # macro 
+  # works in conjunction w/ gem bcrypt and gives us all of those abilities in a secure way that doesn't actually store the plain text password
+  has_secure_password
 end
